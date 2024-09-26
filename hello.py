c0e1c46f7b295c1e96c3f1a072cc04ec99798029
Ines Qian <inesqyx@gmail.com>
2024-09-26 17:56:42 -0400

    Modify Activity 1.3 -2 (example 3-5): Make templates/user.html the default page

--- hello.py
+++ hello.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+def user():
+    return render_template('user.html')
